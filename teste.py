19a604ffb57504ed116e42b9e2e4b50d31cb330f
Josir Cardoso Gomes <josircg@gmail.com>
2026-04-28 15:30:31 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title('Teste ECMI 2')
 st.write("Esse é o meu texto")
+
+nome = st.text_input('Digite o seu nome')
+if nome:
+    print(nome, 'é um cara legal!')
+
+st.image('https://github.com/josircg/projeto1/blob/main/praia.webp')
